4b7d120dd9a7d4c478cb79f90052dc56d79cd8c6
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-28 20:18:58 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,15 +1,52 @@
-def project = 'quidryan/aws-sdk-test'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-branches.each {
-    def branchName = it.name
-    def jobName = "${project}-${branchName}".replaceAll('/','-')
-    job(jobName) {
+def jobs = (1..4)
+def childlist = "'CHILD_1','CHILD_2','CHILD_3','CHILD_4'"
+def name = []
+jobs.each {name.add("CHILD_$it")}
+
+job("MAIN") {
+        label ('EPBYMINW6122')
+        description("This is main")
+        parameters {choiceParam("branch", ["achernak", "master"], "")
+        activeChoiceParam('CHILD') {
+           description('Choose child builds')
+           choiceType('CHECKBOX')
+                groovyScript {script("return[$childlist]")}}}
         scm {
-            git("git://github.com/${project}.git", branchName)
+        git {
+            remote {
+                github("AlexandrSher/dsl", "https")
+            }
+            branch("\$branch")
         }
-        steps {
-            maven("test -Dproject.name=${project}/${branchName}")
-        }
-    }
 }
+        disabled(false)
+        concurrentBuild(false)
+        steps {downstreamParameterized {
+                        trigger('$CHILD') {
+                                block { buildStepFailure('FAILURE')
+                                        unstable('FAILURE')
+                                        failure('UNSTABLE')}
+                                parameters {predefinedProp('branch', '$branch')}}}}
+        wrappers {preBuildCleanup()}}
+
+name.each {
+job("$it") {
+        label ('EPBYMINW6122')
+        description("THIS is child")
+        parameters {choiceParam("branch", ["achernak", "master"], "")
+        keepDependencies(false)
+        scm {
+        git {
+            remote {
+                github("AlexandrSher/dsl", "https")
+            }
+            branch("\$branch")
+        }
+}
+        disabled(false)
+        concurrentBuild(false)
+        steps {shell("""chmod +x script.sh
+bash -ex script.sh > output.txt
+tar czvf \$BUILD_TAG.tar.gz output.txt jobs.groovy
+mv \$BUILD_TAG.tar.gz /var/lib/jenkins/workspace/MAIN/""")}}
+wrappers {preBuildCleanup()}}}
