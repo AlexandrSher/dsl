a8bcbac8a56162719f3f1649a15bcd080f40b0bd
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-28 16:36:42 +0300

    7

--- jobs.groovy
+++ jobs.groovy
@@ -3,33 +3,31 @@ def childlist = []
 jobs.each {childlist.add("CHILD_$it")}
 
 job("MAIN") {
-	description("This is main")
-	parameters {choiceParam("branch", ["achernak", "master"], "")
-	activeChoiceParam('BUILDS_TRIGGER') {
+        description("This is main")
+        parameters {choiceParam("branch", ["achernak", "master"], "")
+        activeChoiceParam('BUILDS_TRIGGER') {
            description('Choose child builds')
            choiceType('CHECKBOX')
-		groovyScript {script("$childlist")}}
-	scm {github("AlexandrSher/dsl", "\$branch")}
-	disabled(false)
-	concurrentBuild(false)
-	childlist.each {
-	steps {downstreamParameterized {
-			trigger("$it") {
-				block {	buildStepFailure("FAILURE")
-					unstable("FAILURE")
-					failure("UNSTABLE")}
-				parameters {predefinedProp('branch', '${branch}')}}}}}	 
+                groovyScript {script("$childlist")}}}
+        scm {github("AlexandrSher/dsl", "\$branch")}
+        disabled(false)
+        concurrentBuild(false)
+        childlist.each {
+        steps {downstreamParameterized {
+                        trigger("$it") {
+                                block { buildStepFailure("FAILURE")
+                                        unstable("FAILURE")
+                                        failure("UNSTABLE")}
+                                parameters {predefinedProp('branch', '${branch}')}}}}}}
 
 childlist.each {
 job("$it") {
-	description()
-	keepDependencies(false)
-	scm {github("AlexandrSher/dsl", "\$branch")}
-	disabled(false)
-	concurrentBuild(false)
-	steps {shell("""chmod +x script.sh 
+        description()
+        keepDependencies(false)
+        scm {github("AlexandrSher/dsl", "\$branch")}
+        disabled(false)
+        concurrentBuild(false)
+        steps {shell("""chmod +x script.sh
 bash -ex script.sh > output.txt
-tar czvf $BUILD_TAG.tar.gz output.txt jobs.groovy 
-mv $BUILD_TAG.tar.gz /var/lib/jenkins/workspace/MAIN/""")}}
-}	
-		   }
+tar czvf $BUILD_TAG.tar.gz output.txt jobs.groovy
+mv $BUILD_TAG.tar.gz /var/lib/jenkins/workspace/MAIN/""")}}}
