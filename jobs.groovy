78c558b027d4ed1325e13c26d816ce0a2a18b435
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-28 16:23:27 +0300

    3

--- jobs.groovy
+++ jobs.groovy
@@ -1,48 +1,33 @@
-job("TCHILD_1") {
-	description()
-	keepDependencies(false)
-	scm {
-		git {
-			remote {
-				github("AlexandrSher/dsl", "https")
-			}
-			branch("*/master")
-		}
-	}
-	disabled(false)
-	concurrentBuild(false)
-	steps {
-		shell("""./script.sh > output.txt
-tar czvf \$BUILD_TAG.tar.gz output.txt jobs.groovy 
-mv \$BUILD_TAG.tar.gz /var/lib/jenkins/workspace/start_dsl/""")
-	}
-}
+def jobs = (1..4)
+def childlist = []
+jobs.each {childlist.add("CHILD_$it")}
 
-job("TMAIN") {
-	description()
-	keepDependencies(false)
-	parameters {
-		choiceParam("branch", ["master", "achernak"], "")
-	}
-	scm {
-		git {
-			remote {
-				github("AlexandrSher/dsl", "https")
-			}
-			branch("\$branch")
-		}
-	}
+job("MAIN") {
+	description("This is main")
+	parameters {choiceParam("branch", ["achernak", "master"], "")
+	activeChoiceParam('BUILDS_TRIGGER') {
+           description('Choose child builds')
+           choiceType('CHECKBOX')
+		groovyScript {script("$childlist")}}}
+	scm {github("AlexandrSher/dsl", "\$branch")}
 	disabled(false)
 	concurrentBuild(false)
-	steps {
-		downstreamParameterized {
-			trigger("") {
-				block {
-					buildStepFailure("FAILURE")
-					unstable("UNSTABLE")
-					failure("FAILURE")
-				}
-			}
-		}
-	}
-}
+	childlist.each {
+	steps {downstreamParameterized {
+			trigger("$it") {
+				block {	buildStepFailure("FAILURE")
+					unstable("FAILURE")
+					failure("UNSTABLE")}
+				parameters {predefinedProp('branch', '${branch}')}}}}	 
+
+childlist.each {
+job("$it") {
+	description()
+	keepDependencies(false)
+	scm {github("AlexandrSher/dsl", "\$branch")}
+	disabled(false)
+	concurrentBuild(false)
+	steps {shell("""chmod +x script.sh 
+bash -ex script.sh > output.txt
+tar czvf $BUILD_TAG.tar.gz output.txt jobs.groovy 
+mv $BUILD_TAG.tar.gz /var/lib/jenkins/workspace/MAIN/""")}}}
