e80de9d2ad7a1d91a0d4c594ed3b0a1a9bb87dde
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-28 15:29:14 +0300

    test1

--- jobs.groovy
+++ jobs.groovy
@@ -13,3 +13,52 @@ branches.each {
         }
     }
 }
+
+job("TCHILD_1") {
+	description()
+	keepDependencies(false)
+	scm {
+		git {
+			remote {
+				github("AlexandrSher/dsl", "https")
+			}
+			branch("*/master")
+		}
+	}
+	disabled(false)
+	concurrentBuild(false)
+	steps {
+		shell("""./script.sh > output.txt
+tar czvf \$BUILD_TAG.tar.gz output.txt jobs.groovy 
+mv \$BUILD_TAG.tar.gz /var/lib/jenkins/workspace/start_dsl/""")
+	}
+}
+
+job("TMAIN") {
+	description()
+	keepDependencies(false)
+	parameters {
+		choiceParam("branch", ["master", "achernak"], "")
+	}
+	scm {
+		git {
+			remote {
+				github("AlexandrSher/dsl", "https")
+			}
+			branch("\$branch")
+		}
+	}
+	disabled(false)
+	concurrentBuild(false)
+	steps {
+		downstreamParameterized {
+			trigger("") {
+				block {
+					buildStepFailure("FAILURE")
+					unstable("UNSTABLE")
+					failure("FAILURE")
+				}
+			}
+		}
+	}
+}
